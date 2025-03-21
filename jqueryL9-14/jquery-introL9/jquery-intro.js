ad68be51f164fd5da4f26938cdff4ca5b6db137a
davidhyson <davidhyson@dallascollege.edu>
2025-03-21 17:32:11 -0500

    months

--- jqueryL9-14/jquery-introL9/jquery-intro.js
+++ jqueryL9-14/jquery-introL9/jquery-intro.js
@@ -1,10 +1,16 @@
 // create a months array
 var months = ['January', 'February','March','April','May','June','July','August','September','October','November', 'December'];
+var month = months[date.getMonth()]
+document.getElementById("month").innerHTML = month;
+
 // create a tips array
 var tips=["<p>Those of  us who live through our gardens know how bittersweet it can be to see the lush green growth of  summer disappear during the cold months of  winter. Hellebores are the perfect winter garden companion to plant with another winter blooming favorite, Camellias.</p><p>Visit us this season for the exciting selection of  both of  these cool season beauties and enjoy them in your garden for years to come.</p>",
 "<p>Spring is a time of  renewal for both the gardener and the garden. Suggestions for this month: </p><ul><li> Transplants of  tomatoes should be planted by March 15th</li><li> Fertilize your  lawn after warm-season grasses are growing and have been mowed 2-3 times.</li><li> Release beneficial nematodes to control ticks, fleas, chiggers, and fire ants. </li></ul>",
 "<p><strong>Summer is the TIME</strong><ul><li>Select rose bushes from the huge selection of  varieties we offer.</li><li>Transplants of  peppers, eggplant and tender herbs can be planted.</li><li>Plant fruit trees, shrubs, roses, perennials, herbs and colorful annuals like geraniums and snapdragons.</li></ul>",
 "<p>Fall is the best time to plant in north Texas. Milder temperatures and increased rainfall mean that new plants—especially trees and shrubs—can establish root systems much more easily than during hot summer months.</p><p>Also with the changing season comes the changing of  our color plantings: petunias, dianthus, ornamental kale and mums bringing cheerful color ahead of  winter’s trusted and long-lasting pansies and violas.</p>"];
+
+
+
 // create a specials array
 var specials = ["<p>Don't forget our feathered friends!</p><p>All bird feeders and birdseed are 50% off this January.</p> ",
 "<p>Roses for your sweetheart!</p><p>All roses are $24.99 this February.</p>",
