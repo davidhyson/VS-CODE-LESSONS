fe88c3ce97f746631b29a332c4f1b8ea6a17f773
davidhyson <davidhyson@dallascollege.edu>
2025-04-06 11:51:31 -0500

    click events

--- jqueryL9-14/jquery-eventsL10/glossary.js
+++ jqueryL9-14/jquery-eventsL10/glossary.js
@@ -1,5 +1,12 @@
+//jquery function is required
 $(document).ready(function(){
-//h1 and h2
+
+//hide the botanic class
+$('.botanic').hide();
+//hide method to the imgdiv class
+$('.imgdiv').hide();
+
+//h1 and h2, add mouseover and mouseout to headings to change color. Graded rubric
  $('h1,h2').mouseover(function(){
     $(this).css('color','red');
 });
@@ -7,7 +14,11 @@ $('h1,h2').mouseout(function(){
    $(this).css('color','black');
 });
 
-
+//flower methond, Hide botanic names and display individual ones when a flower is clicked. Graded Rubric
+$('.flower').click(function(){
+    $('.botanic').hide();
+    $(this).children('.botanic').show();
+});
 
 
 });
