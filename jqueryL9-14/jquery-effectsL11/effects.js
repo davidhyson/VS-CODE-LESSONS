652ca7c28fdc5ce0779a63ac939a3454d55d1b2b
davidhyson <davidhyson@dallascollege.edu>
2025-04-12 19:04:52 -0500

    init

--- jqueryL9-14/jquery-effectsL11/effects.js
+++ jqueryL9-14/jquery-effectsL11/effects.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
         });
     });
     $('h2').mouseout(function(){
-        $('#slogan').fadeOut(function() {
+        $('#slogan').fadeOut(fast, function() {
             var $h2 = $('#slogan');
             $h2.text('The Powers of Flowers'); 
             $('#slogan').show();
