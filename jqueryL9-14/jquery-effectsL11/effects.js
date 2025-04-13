1fac51fa49438ccf39702aa573d3b474de1d507e
davidhyson <davidhyson@dallascollege.edu>
2025-04-13 11:13:37 -0500

    cleanup

--- jqueryL9-14/jquery-effectsL11/effects.js
+++ jqueryL9-14/jquery-effectsL11/effects.js
@@ -9,38 +9,38 @@ $(document).ready(function() {
         $('#newsSignup').slideToggle();
 
         // Change + to - and vice versa
-        let sign = $("#openclose").text();
-        $("#openclose").text(sign === "+" ? "-" : "+");
+        let sign = $('#openclose').text();
+        $('#openclose').text(sign === "+" ? "-" : "+");
         
     });
 
     //h2 slogan,
     //Use fadeIn() and fadeOut() and callback for the slogan and change the text. Graded rubric
-    $("#slogan").hover(
+    $('#slogan').hover(
         function () {
-          $(this).fadeOut("normal", "linear", function () {
-            $(this).text("Hand Picked Just for You").fadeIn("slow", "swing");
+          $(this).fadeOut('normal', 'linear', function () {
+            $(this).text("Hand Picked Just for You").fadeIn('slow', 'swing');
           });
         },
         function () {
-          $(this).fadeOut("fast", "swing", function () {
-            $(this).text("The Power of Flowers").fadeIn("slow", "linear");
+          $(this).fadeOut('fast', 'swing', function () {
+            $(this).text("The Power of Flowers").fadeIn('slow', 'linear');
           });
         }
      );
     
-    //Animate the rose image on page load
+    //Animate the rose image on page load pg.193
     $('#rose').animate({
         right: '100px',
         opacity: 1
     }, 1500, 'swing');
 
-    //Form submission event
+    //Form submission event pg.258
     //Add submit event to display the alert, hide form, fade out link and prevent form submission. Graded rubric
-    $("#newsSignup").submit(function (event) {
+    $('#newsSignup').submit(function (event) {
         alert("Thank you for registering");
         $(this).hide();
-        $("#signuplink").fadeTo("slow", 0.3);
+        $('#signuplink').fadeTo('slow', 0.3);
         event.preventDefault();
     });
     
