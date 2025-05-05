7b2b60664095d705328d86f5b0cc345675293ac7
davidhyson <davidhyson@dallascollege.edu>
2025-05-05 13:48:02 -0500

    val3

--- jqueryL9-14/jquery-orderformL14/orderform.js
+++ jqueryL9-14/jquery-orderformL14/orderform.js
@@ -126,7 +126,7 @@ $(document).ready(function() {
     // set valid flag to true
     valid = true;
     // validate Grand Total
-    if (orderTotal == "") {
+    if ($('#gTotal').val() == "") {
       $('#qtytotalErr').text('Please enter QTY');
       valid = false; // set valid flag to false if error found
     } else  {
